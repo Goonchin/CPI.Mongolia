356a275bcfd756a5e0f82bf44ea1e1aa973c8fcf
Chinguun Batsaikhan <90230796+Goonchin@users.noreply.github.com>
2022-12-23 22:04:26 +0800

    Update CPI.py

--- CPI.py
+++ CPI.py
@@ -9,9 +9,9 @@ from prophet.diagnostics import cross_validation
 from prophet.plot import plot_cross_validation_metric
 #for encoding binary data to printable ASCII characters and decoding it  #back to binary form
 import base64 
-    st.title('Time Series Forecasting Using Streamlit')
-    st.write("IMPORT DATA")
-    st.write("Import the time series CSV file. It should have two columns labelled as 'ds' and 'y'. The 'ds' column should be of DateTime format by Pandas. The 'y' column must be numeric representing the measurement to be forecasted.") 
+st.title('Time Series Forecasting Using Streamlit')
+st.write("IMPORT DATA")
+st.write("Import the time series CSV file. It should have two columns labelled as 'ds' and 'y'. The 'ds' column should be of DateTime format by Pandas. The 'y' column must be numeric representing the measurement to be forecasted.") 
 
 if data is not None:
     appdata = pd.read_csv(data)  #read the data fro
