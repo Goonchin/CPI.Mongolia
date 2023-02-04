08594bdcb7bba3adff38cda9d1cea2739670c591
Chinguun Batsaikhan <90230796+Goonchin@users.noreply.github.com>
2023-02-04 20:41:01 +0800

    Update CPI.py

--- CPI.py
+++ CPI.py
@@ -48,3 +48,27 @@ st.write("Analysing the inflation over the years and how they relate to the aver
 # Plot the predictions
 fig =model.plot(forecast)
 st.pyplot(fig)
+
+if selected_column == "Alcohol And Tobacco":
+    description = "The price of alcohol and tobacco products in Mongolia over the next 36 months, will continute to increase due to inflation and overall increasing costs for materials and labor."
+elif selected_column == "Clothing,Footwear,and Cloth":
+    description = "The price of clothing, footwear, and cloth products in Mongolia over the next 36 months will continute to increase due to inflation and overall increasing costs for materials and labor."
+elif selected_column == "Communications":
+    description = "The price of communication in Mongolia over the next 36 months will continute to stay stagnant due to relative costs over the years, showing not much change overall."
+elif selected_column == "Education Services":
+    description = "The price of education services in Mongolia over the next 36 months will continute to increase due to inflation and overall increasing costs for labor and supplies."
+elif selected_column == "Food And Non-Alcoholic Beverages":
+    description = "The price of food and non-alcoholic beverage products in Mongolia over the next 36 months will continute to increase due to inflation and overall increasing costs for materials and labor."
+elif selected_column == "Health":
+    description = "The price of health products and services in Mongolia over the next 36 months will continute to increase due to inflation and overall increasing costs for materials and labor."
+elif selected_column == "Housing,Water,Electricity,Fuels":
+    description = "The price of housing,water,electricity and fuels in Mongolia over the next 36 months will continute to increase due to inflation, scarcity and maintenance costs."
+elif selected_column == "Overall Index":
+    description = "The prices of the overall index costs will continue to increase, due to an increase of all the difference consumer goods and services."
+elif selected_column == "Recreation and Culture":
+    description = "The price for recreation and culture will continue to increase due to levels of high tourism and overall costs for recreational activities increase as well."
+elif selected_column == "Transport":
+    description = "The price for transport might decrease, due to the high influctution the past several months, but they also have a high chance of increasing due to other costs increaseing."
+# Add more elif blocks for other columns with their respective descriptions
+else:
+    description = "The price of the selected product category in Mongolia over the next 36 months, will overall increase over the years."
