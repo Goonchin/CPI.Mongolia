c6c80b5007b0b0eaded52c8d079e1a1440e22412
Chinguun Batsaikhan <90230796+Goonchin@users.noreply.github.com>
2023-02-04 19:18:38 +0800

    Update CPI.py

--- CPI.py
+++ CPI.py
@@ -20,3 +20,4 @@ columns = ['Alcohol And Tobacco', 'Clothing,Footwear,and Cloth'] # add other col
 selected_column = st.selectbox("Select a column", columns)
 
 df = load_data(selected_column)
+return df
