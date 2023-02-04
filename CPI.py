3639366db7c841aae7f4e7029dea454a722ebdb7
Chinguun Batsaikhan <90230796+Goonchin@users.noreply.github.com>
2023-02-04 19:41:38 +0800

    Update CPI.py

--- CPI.py
+++ CPI.py
@@ -21,16 +21,17 @@ def load_data(column):
 
 df = load_data(selected_column)
 
-df.index = df['ds']
-df = df[['y']]
-df.rename(columns={'y': 'y'}, inplace=True)
-df['ds'] = df.index
+df.columns
+#df.index = df['ds']
+#df = df[['y']]
+#df.rename(columns={'y': 'y'}, inplace=True)
+#df['ds'] = df.index
 
-model = Prophet()
-model.fit(df)
+#model = Prophet()
+#model.fit(df)
 
 # Make predictions
-future = model.make_future_dataframe(periods=12, freq='M')
-forecast = model.predict(future)
+#future = model.make_future_dataframe(periods=12, freq='M')
+#forecast = model.predict(future)
 
-st.write(forecast)
+#st.write(forecast)
