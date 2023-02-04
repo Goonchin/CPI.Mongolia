abe5ec0254566771f175d40194feeca3d845f942
Chinguun Batsaikhan <90230796+Goonchin@users.noreply.github.com>
2023-02-04 19:20:16 +0800

    Update CPI.py

--- CPI.py
+++ CPI.py
@@ -4,6 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from prophet import Prophet
 
+st.title("Consumer Price Index in Mongolia")
+st.write("Analysing the inflation over the years and how they relate to the average prices of goods and services in Mongolia, using Facebook Prophet. By using past recorded data to make predictions for the price changes for the next 12 months.")
+
+columns = ['Alcohol And Tobacco', 'Clothing,Footwear,and Cloth'] # add other columns here
+selected_column = st.selectbox("Select a column", columns)
+
 @st.cache
 def load_data(column):
     if column == "Alcohol And Tobacco":
@@ -11,13 +17,4 @@ def load_data(column):
     elif column == "Clothing,Footwear,and Cloth":
         df = pd.read_csv("Data/clothing, footwear, and cloth.csv", thousands=",")
     # add more elif blocks for other columns
-    return df
-
-st.title("Consumer Price Index in Mongolia")
-st.write("Analysing the inflation over the years and how they relate to the average prices of goods and services in Mongolia, using Facebook Prophet. By using past recorded data to make predictions for the price changes for the next 12 months.")
-
-columns = ['Alcohol And Tobacco', 'Clothing,Footwear,and Cloth'] # add other columns here
-selected_column = st.selectbox("Select a column", columns)
-
-df = load_data(selected_column)
     return df
