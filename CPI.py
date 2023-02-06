548182a98272e5e35d8dbe9069956bda192195fb
Chinguun Batsaikhan <90230796+Goonchin@users.noreply.github.com>
2023-02-06 17:08:08 +0800

    Update CPI.py

--- CPI.py
+++ CPI.py
@@ -52,7 +52,7 @@ fig.update_layout(xaxis_title="Date", yaxis_title="Price")
 st.plotly_chart(fig)
 
 if selected_column == "Alcohol And Tobacco":
-    description = "The price of alcohol and tobacco products in Mongolia over the next 36 months, will continue to increase due to inflation and overall increasing costs for materials and labor."
+    description = "The recorded data starts in 2006-2022. Using Prophet we can make a prediction for the next few years, which is shown in the above visual. The price of alcohol and tobacco products in Mongolia over the next 36 months, will continue to increase due to inflation and overall increasing costs for materials and labor."
 elif selected_column == "Clothing,Footwear,and Cloth":
     description = "The price of clothing, footwear, and cloth products in Mongolia over the next 36 months will continue to increase due to inflation and overall increasing costs for materials and labor."
 elif selected_column == "Communications":
